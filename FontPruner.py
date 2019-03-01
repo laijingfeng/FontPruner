0c88fe7d929939e9f347c321b1e8b475d5ee5043
laijingfeng <337292616@qq.com>
2019-03-01 12:41:00 +0800

    整理

--- FontPruner.py
+++ FontPruner.py
@@ -1,55 +1,76 @@
-"""Usage:
-  FontPruner.py --inputPath=<inputPaths>... --inputFont=<inputFont>...  [--tempPath=<tempPath>]
+# !/usr/bin/python
+# encoding=utf-8
+# version: 2019-03-01
+
 """
+Usage:
+FontPruner.py --inputPath=<inputPaths>... --inputFont=<inputFont>...  [--tempPath=<tempPath>]
+"""
+
 from docopt import docopt
 import os
 
-SepPath = os.path.sep
+SepPath = os.path.sep # 分隔符
 TempPathDefault = "tmp"
-InputFilelist = "input_filelist.txt"
-IntermediateFolder = "intermediate"
-OutputFolder = "output"
-ChineseOutPut = "ChineseOutPut.txt"
-UnChineseOutPut = "unChineseOutPut.txt"
+InputFilelist = "input_filelist.txt" # 白名单文件列表记录文件
+IntermediateFolder = "intermediate" # 白名单汇总目录
+OutputFolder = "output" # 新字体输出目录
+ChineseOutPut = "ChineseOutPut.txt" # 白名单汇总文件_汉字
+UnChineseOutPut = "unChineseOutPut.txt" # 白名单汇总文件_非汉字
 Succ = 0
-def genFilePathList(inputPath,FileListOP):    
+
+def genFilePathList(inputPath, FileListOP):
+  """
+  收集白名单文件列表\n
+  inputPath - 白名单目录\n
+  FileListOP - 输出临时目录
+  """
+  print "==收集白名单文件列表 开始=="
   fullPara = ""
   for path in inputPath:
-    print "path = " +path
-    fullPara +=path+" "
-  fullPara += " "+FileListOP + SepPath+InputFilelist    
-  command ="java -jar bin"+SepPath+"GenFileList.jar " +fullPara
+    fullPara += path + " "
+  fullPara += " " + FileListOP + SepPath + InputFilelist    
+  command ="java -jar bin" + SepPath + "GenFileList.jar " + fullPara
   if os.system(command) is not Succ:
     raise Exception('generate fileList.txt error!'+command)
-  print("genFilePathList completed" +"   " +fullPara)
-
+  print("--收集白名单文件列表 完成--")
 
 def extractFileString(temp):
-  fileListPath = temp+SepPath+InputFilelist
-  outputPath = temp+SepPath+IntermediateFolder
+  """
+  白名单内容汇总\n
+  temp - 输出临时目录
+  """
+  print "==白名单内容汇总 开始=="
+  fileListPath = temp + SepPath + InputFilelist
+  outputPath = temp + SepPath + IntermediateFolder
 
-  command ="java -jar bin"+SepPath+"fontExtract.jar " +fileListPath + " "+outputPath
+  command ="java -jar bin" + SepPath + "fontExtract.jar " + fileListPath + " "+ outputPath
   if os.system(command) is not Succ:
     raise Exception('extract font string  error!'+command)
-  print("extractFileString completed")
+  print("--白名单内容汇总 完成--")
 
-
-def bulidNewFont(originPath,outPutPath):
-    fullOutPut = outPutPath+SepPath+OutputFolder
-    if not os.path.exists(fullOutPut):
-      os.makedirs(fullOutPut)
-    for fontOrigin in originPath:
-      index = len(fontOrigin) - fontOrigin.rfind(SepPath)
-      fontName = fontOrigin[-index:]
-      print ("fontName" +fontName)
-      fullPara = ""
-      fullPara += outPutPath+SepPath+IntermediateFolder+SepPath+ChineseOutPut+"  "+outPutPath+SepPath+IntermediateFolder+SepPath+UnChineseOutPut+" "+fontOrigin + " " + fullOutPut+SepPath+fontName 
-      command ="java -jar bin"+SepPath+"sfnttool.jar -c " +fullPara
-      
-      if os.system(command) is not Succ:
-        raise Exception('build new font error!'+command)
-      print("bulidNewFont completed"+ fontOrigin)
-
+def bulidNewFont(originPath, outPutPath):
+  """
+  生成新字体\n
+  originPath - 输入字体文件列表\n
+  outPutPath - 输出临时目录
+  """
+  print "==生成新字体 开始=="
+  fullOutPut = outPutPath + SepPath + OutputFolder
+  if not os.path.exists(fullOutPut):
+    os.makedirs(fullOutPut)
+  for fontOrigin in originPath:
+    index = len(fontOrigin) - fontOrigin.rfind(SepPath)
+    fontName = fontOrigin[-index:]
+    print ("字体文件：" + fontName)
+    fullPara = ""
+    fullPara += outPutPath + SepPath + IntermediateFolder + SepPath+ChineseOutPut + "  " + outPutPath + SepPath + IntermediateFolder + SepPath+UnChineseOutPut + " " + fontOrigin + " " + fullOutPut + SepPath + fontName
+    command ="java -jar bin" + SepPath + "sfnttool.jar -c " + fullPara
+    
+    if os.system(command) is not Succ:
+      raise Exception('build new font error!'+command)
+    
+    print("--生成新字体 完成--")
 
 if __name__ == '__main__':
   # arguments = docopt(__doc__, version='0.1.1rc')
@@ -59,7 +80,7 @@ if __name__ == '__main__':
   arguments = {} # 参数列表
   arguments['--inputPath'] = ['input'] # 白名单目录
 
-  arguments['--inputFont'] = [] # 字体目录
+  arguments['--inputFont'] = [] # 输入字体文件列表
   font_path = './fonts/'
   list = os.listdir(font_path)
   for line in list:
@@ -87,7 +108,7 @@ if __name__ == '__main__':
         raise Exception("tempPath - bad path: " + tmp) 
 
   genFilePathList(arguments['--inputPath'], tmp) 
-
+  print('')
   extractFileString(tmp)
-  
+  print('')
   bulidNewFont(arguments['--inputFont'], tmp)
